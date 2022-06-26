d9d33cb954e08d3954f559466fc76bbde680bbc6
Rahul Mishra <rahulmishra.kv@gmail.com>
2022-06-26 11:58:32 +0530

    Added: Simple Tests

--- src/test/java/tests/ExampleTest.java
+++ src/test/java/tests/ExampleTest.java
@@ -1,19 +1,58 @@
 package tests;
 
-import com.microsoft.playwright.*;
-import org.assertj.core.api.Assertions;
-
-import java.io.IOException;
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
 public class ExampleTest {
+    Playwright playwright;
 
-    public static void main(String[] args) {
-        Playwright playwright = Playwright.create();
+
+    @BeforeClass
+    public void beforeClass() {
+        playwright = Playwright.create();
+    }
+
+
+    @Test
+    public void googleTest() {
         Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
         Page page = browser.newPage();
         page.navigate("http://www.google.com");
         System.out.println(page.title());
-        //AssertJ assertion
-        Assertions.assertThat(page.title().equalsIgnoreCase("Google!"));
+        Assert.assertEquals(page.title(), "Google");
+        browser.close();
     }
+
+    @Test
+    public void firefoxTest() {
+        Browser browser = playwright.firefox().launch(new BrowserType.LaunchOptions().setHeadless(false));
+        Page page = browser.newPage();
+        page.navigate("http://www.google.com");
+        System.out.println(page.title());
+        Assert.assertEquals(page.title(), "Google");
+        browser.close();
+    }
+
+    @Test
+    public void safariTest() {
+        Browser browser = playwright.webkit().launch(new BrowserType.LaunchOptions().setHeadless(false));
+        Page page = browser.newPage();
+        page.navigate("http://www.google.com");
+        System.out.println(page.title());
+        Assert.assertEquals(page.title(), "Google");
+        browser.close();
+    }
+
+
+    @AfterClass
+    public void afterClass() {
+        playwright.close();
+    }
+
 }
